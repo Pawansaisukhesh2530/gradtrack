2ba1b22765ec756f73ed0745c2dae74a4d861b10
Raminisetty Pawan sai sukhesh <raminisettypawansaisukhesh@gmail.com>
2025-06-27 11:58:12 +0530

    Delete gradtrack/gradtrack/transcript_app directory

--- gradtrack/gradtrack/transcript_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Student)
-admin.site.register(Event)
-admin.site.register(Participation)
-admin.site.register(GraduateAttribute)
-admin.site.register(CharacterStrength)
-admin.site.register(AttributeStrengthMap)
